d20a65bf928c88ce80873ca489ea85aaab7fb5f1
MANOJG122 <manoj89evils@gmail.com>
2024-02-01 10:18:46 +0530

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+ int input()
+ {
+    int n;
+    printf("enter the numbers:");
+    scanf("%d",&n);
+    return n;
+ }
+ int compare(int a,int b, int c)
+ {
+    int lar;
 
+    if (a > b && a > c) {
+        lar = a;
+    } else if (b > a && b > c) {
+        lar = b;
+    } else {
+        lar = c;
+    }
+
+    return lar;
+ }
+ void output(int a, int b, int c, int lar)
+ {
+    printf("the largest of %d %d & %d is: %d",a,b,c,lar);
+ }
+ int main()
+ {
+    int a=input();
+    int b=input();
+    int c=input();
+    int largest=compare(a,b,c);
+    output(a,b,c,largest);
+    return 0;
+
+ }
